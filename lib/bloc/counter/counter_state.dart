ac778e5b96ee90c46faafb517596147c656fac50
Thitphavanh <thitphavanh.psv@gmail.com>
2022-05-04 14:00:05 +0700

    update

---
+++ lib/bloc/counter/counter_state.dart
@@ -0,0 +1,29 @@
+part of 'counter_bloc.dart';
+
+class CounterState extends Equatable {
+  final int counter;
+
+  CounterState({
+    required this.counter,
+  });
+
+  factory CounterState.initial() {
+    return CounterState(counter: 0);
+  }
+
+  @override
+  List<Object> get props => [];
+
+  @override
+  String toString() => 'CounterState(counter: $counter)';
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
+}
+
+
